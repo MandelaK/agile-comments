666b118816eda77717e5a69ec937221258645c62
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-19 13:29:12 +0300

    [ft #162045695]add the sign_up interface

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
 from db_config import create_tables
+from app.views import UserView
 
 create_tables()
 
-print("Welcome to our program . Be awesome ")
+print("Welcome to our program . Be awesome\n")
+
+signed_up = input("Are you registered if yes enter y if not enter any word\n")
+
+signed_up_lower = signed_up.lower()
+
+data = False
+while data is not True:
+    if signed_up_lower != "y":
+        print("You will be required to sign-up\n")
+        name = input("please enter your name\n")
+        password = input("please enter your password\n")
+        user = UserView()
+        signup = user.UserView(name, password)
+        if not signup:
+            print("User name exist change the user_name")
+            data = False
+        else:
+            data = True
+    else:
+        pass
+print("Please Enter your user_name and password")
+
+
+
+
+
+
+
+
+
+
 
